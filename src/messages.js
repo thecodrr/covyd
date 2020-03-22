e6c903580fe1db9503d1185961a576eac8947c05
thecodrr <thecodrr@protonmail.com>
2020-03-22 23:07:13 +0500

    feat: reduce contribution message frequency

--- src/messages.js
+++ src/messages.js
@@ -11,7 +11,7 @@ const messages = [
 
 function getRandomMessage() {
   let message = messages[Math.floor(Math.random() * messages.length)];
-  if (Math.random() * 100 > 60)
+  if (Math.random() * 100 > 40)
     message += rgb.it(
       "\nYou can contribute your own personal messages, tips, jokes or stories by going to https://github.com/thecodrr/covyd"
     );
